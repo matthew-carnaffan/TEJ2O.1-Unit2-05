4349c5e88d0b23fc77495b2093bdaaf0bf11e7bc
matthew-carnaffan <matthew.carnaffan@stu.ocsb.ca>
2026-02-24 12:46:01 -0500

    removed hello world and changed titles

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Matthew C
+ * Created on: Feb 2026
+ * This program tell the tempurature in kelvin
 */
 
-basic.showString('Hello, World!')
+
